916b74c9f6d264a1cb01a7f4294a65f2e88c33a0
Archimedix <io@archimedix.net>
2020-04-01 18:38:13 +0000

    Initial commit

---
+++ ricette-thai/Ricette_thai_files/Ricette_thai.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20120603
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_1:new IWShadow({blurRadius:2,offset:new IWPoint(1.4142,1.4142),color:'#22031f',opacity:0.750000}),stroke_0:new IWEmptyStroke(),shadow_2:new IWShadow({blurRadius:4,offset:new IWPoint(1.4142,1.4142),color:'#ff1bea',opacity:0.750000}),shadow_3:new IWShadow({blurRadius:10,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.750000}),reflection_0:new IWReflection({opacity:0.50,offset:2.00}),shadow_4:new IWShadow({blurRadius:10,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.750000}),shadow_0:new IWShadow({blurRadius:5,offset:new IWPoint(2.1213,2.1213),color:'#e225d5',opacity:0.750000})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Ricette_thai_files/Ricette_thaiMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');fixAllIEPNGs('Media/transparent.gif');applyEffects()}
